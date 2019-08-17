e67fa3c86c92df67376424195123191b32bf436a
Hoo Man <hoomanfor@gmail.com>
2019-08-17 16:20:19 -0400

    placeholder function prints hyphens for each letter in correct lyric

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,13 +8,28 @@ var wins = 0;
 var losses = 0;
 var guesses = 10;
 var lettersGuessed = [];
+var correctLetters = [];
+
+var domPlaceholder = document.querySelector("#placeholder");
+
+function placeholder() {
+    for (var i = 0; i < correctLyric.length; i++) {
+        domPlaceholder.textContent += " - ";
+    }
+}
 
 document.addEventListener("keyup", function() {
     var guess = event.key 
     for (var i = 0; i < correctLyric.length; i++) {
         if (correctLyric[i] == guess) {
+            correctLetters.push(correctLyric[i]);
             console.log("Correct letter found in lyrics: " + correctLyric[i])
             console.log ("What is the indexOf: " + correctLyric.indexOf(correctLyric[i]))
+            console.log ("correctLetters Array: ", correctLetters);
         }
     }
 })
+
+placeholder();
+
+
