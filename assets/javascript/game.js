dea2550774df73db77b8b786475ce86665b102d7
Hooman Foroudastan <hforoudastan@hforoudastan-wifi.rdu.lulu.com>
2019-08-19 17:18:09 -0400

    correctGuess array contains hyphens and correct guesses replace them at correct index

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,6 @@ var lyrics = ["red", "green", "blue", "orange", "purple", "black", "blue", "yell
 var correctLyric = lyrics[Math.floor(Math.random() * lyrics.length)];
 console.log("correctLyric: " + correctLyric);
 
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 var wins = 0;
 var losses = 0;
 var guesses = 10;
@@ -15,24 +13,19 @@ var placeholder = [];
 
 var domPlaceholder = document.querySelector("#placeholder");
 
+for (var j = 0; j < correctLyric.length; j++) {
+    correctGuess.push(" - ");
+}
 
 
 document.addEventListener("keyup", function() {
-        placeholder = [];
-        for (var j = 0; j < correctLyric.length; j++) {
-            placeholder.push(" - ");
-        }
-        correctLetters = [];
         var guess = event.key;
         for (var i = 0; i < correctLyric.length; i++) {
             if (correctLyric[i] === guess) {
-                correctGuess[i] = correctLyric[i];
-            } else {
-                console.log("Incorrect Answer")
+                correctGuess[i] = guess;
             }
         }
         console.log("correctGuess Array: " + correctGuess);
-        console.log("placeholder: " + placeholder)
 })
 
 
